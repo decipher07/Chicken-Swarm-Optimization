4ce19383cca7b4b500e0aac50e411209675c5c70
decipher07 <social.deej@gmail.com>
2020-05-09 22:15:45 +0530

    Understanding Algo

---
+++ script.py
@@ -0,0 +1,20 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import random
+import pandas as py 
+import io
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
+
+ds = py.read_csv('kag_risk_factors_cervical_cancer.csv')
+
+print(ds)
+
+r = np.random.random(32)
+# print(r)
+
+x_sample = np.random.randint(0,2,(1,32))
+print(x_sample)
+
+df = np.zeros((10, 5))
+print(df)
