a51e5407de667969b82c9783e2e14366b6905873
decipher07 <social.deej@gmail.com>
2020-05-10 05:26:57 +0530

    Added Description to Chicken Class

--- CSOAlgo.py
+++ CSOAlgo.py
@@ -1,9 +1,7 @@
 import numpy as np 
 import matplotlib 
-import random 
-import pandas as pd 
-import sklearn
-
+import math
+import random
 ''' 
     The Fitness Function Defines The Fitness of the indiviual in the Group. The Group's Leader ie Rooster is the most Fittest in The Group to Which it Accounts as the Reason
 '''
@@ -18,7 +16,7 @@ def fitness_function (x):
     '''
     w = 0.00000001
     E = 0.00000002
-    N = 32 
+    N = 32 # For an Example 
     
     sum = 0 # Sum For Countering The Total No Of Non Negative Numbers in The Dataset
 
@@ -26,11 +24,27 @@ def fitness_function (x):
         if i == 1 :
             sum+=1
     
+    ''' 
+        The Fitness Value is Defined by the Given Formula
+        The Expression Yields Different Results for Different Value of w and E 
+    '''
+
     fitness_value  = w*E + ((1-w)*sum)/N
 
     return fitness_value 
 
-x = np.random.randint(0, 2, (1, 32))
-fitness =  fitness_function(x)
+# x = np.random.randint(0, 2, (1, 32))
+# fitness =  fitness_function(x)
 
-print (fitness)
+''' 
+    Understanding The Algo , We have 
+
+    Species : Chicken 
+    Members : Roosters (The Head Male Chicken)
+              Hens (The Female Chickens)
+              Chicks (The Baby Chickens )
+
+
+    Thereby , A Class Containing Them Will Define Appropriate Properties and Updation 
+
+'''
