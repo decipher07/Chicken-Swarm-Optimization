fb64adfa928d66ac7971f5924ac407014dd4ab2e
decipher07 <social.deej@gmail.com>
2020-05-10 20:39:21 +0530

    Added up the Updation For Hens

--- CSOAlgo.py
+++ CSOAlgo.py
@@ -108,5 +108,42 @@ class Chicken :
             ## Checking If It doesnt' Belong to the same Group 
             random_number_between_the_total_number_of_groups = np.random.randint(0, number_of_groups_the_swarm_is_divided) 
         
-        ### Evaluating The Different Function 
+        ### Evaluating The Equation According to The Algorithm 
 
+        ## Initalizing Sigma 
+        sigma_square = 0
+        e = 0.000000000000000000000000000000000001
+        if rooster[random_number_between_the_total_number_of_groups].group != self.group :
+            if rooster[random_number_between_the_total_number_of_groups].fitness >= self.fitness:
+                sigma_square = 1 
+            else :
+                sigma_square = np.exp((rooster[random_number_between_the_total_number_of_groups].fitness - self.fitness)/ (np.abs(self.fitness) + e ))
+
+        # Create Gaussian Distribution  with Mean 0 and Standard Deviation is sigma_sqare 
+        random_distribution = np.random.normal(0, sigma_square)
+
+        '''
+            We are Only Updating The Next Position , And Not the Original Position , Because the Update is Valid only when The Original Fitness is found lowered to The Mutated Fitness
+        '''
+
+        for index in range (0, Dn):
+            self.next_position_to_which_chicken_will_move[index] = self.original_position[index]*(1+random_distribution)
+
+
+    def update_location_hen(self, number_of_groups_the_swarm_is_divided, rooster): # Integer , Class Rooster
+        ''' 
+            The Rooster being Passed as a Parameter is to locate the Group to which the particular Rooster Belongs To , Including The Various Other Position Avaiable 
+        '''
+        
+        for index in range (0, number_of_groups_the_swarm_is_divided):
+            # Running a For Loop , Since THe The Number of Groups are meant to be less than the original Population Created 
+            # Check if the Rooster of that Group Matches , Our Hens Group or Not 
+            # Since According to Segragation of The Population in Group , It is meant that eqaul Population will be shared 
+            if rooster[index].group == self.group :
+                position_rooster_1 = rooster[index].position # Same Group Rooster Position  
+                fitness_rooster_1 = rooster[index].fitness # Same Group Rooster Health 
+            
+            # Generating A Random Number in range of Groups
+            random_number_between_the_total_number_of_groups = np.random.randint(0, number_of_groups_the_swarm_is_divided) # Example as Like if the Population is divided into 2 Groups , and then Total Option is Limited to either 0 or 1 , 0 for the First Group and 1 For Other Group 
+
+            while (rooster[random_number_between_the_total_number_of_groups].group == self.group)
